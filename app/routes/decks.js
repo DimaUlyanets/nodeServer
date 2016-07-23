5f5b2e7a88ca8353e6a2c041bbef8f77a3ec7333
Dmytro Ulyanets <dulyanets@lixar.com>
2016-07-23 20:58:53 -0300

    fast fixing

--- app/routes/decks.js
+++ app/routes/decks.js
@@ -1,26 +1,29 @@
 var express = require('express');
 
 var Deck = require('./app/models/deck');
+module.exports = function (router) {
+    'use strict';
 
-express.Router.route('/decks')
-    .post(function (req, res) {
-        var deck = new Deck();
-        console.log('New deck', req.body.question)
-        deck.name = req.body.name;
+    router.route('/decks')
+        .post(function (req, res) {
+            var deck = new Deck();
+            console.log('New deck', req.body.question)
+            deck.name = req.body.name;
 
-        deck.save(function (err) {
-            if (err) {
-                res.send(err);
-            }
+            deck.save(function (err) {
+                if (err) {
+                    res.send(err);
+                }
 
-            res.json({message: 'Deck created.'})
+                res.json({message: 'Deck created.'})
+            });
+        })
+        .get(function (req, res) {
+            Deck.find(function (err, decks) {
+                if (err) {
+                    res.send(err);
+                }
+                res.json(decks);
+            });
         });
-    })
-    .get(function (req, res) {
-        Deck.find(function (err, decks) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(decks);
-        });
-    });
+}

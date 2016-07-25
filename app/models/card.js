e2124aae622b718f869bcab1e3b78596579ee818
Dmytro Ulyanets <dulyanets@lixar.com>
2016-07-25 14:39:17 -0300

    added objectid ref

--- app/models/card.js
+++ app/models/card.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 
 var CardSchema = new Schema({
     question: String,
-    answer: String
+    answer: String,
+    deckId: Schema.Types.ObjectId
 });
 
 module.exports = mongoose.model('Card', CardSchema);
